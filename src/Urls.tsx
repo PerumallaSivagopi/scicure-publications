0c9084f628c84e214e979855e4e8f9ccfa0ef051
PerumallaSivagopi <perumallasivagopi1234@gmail.com>
2025-12-25 16:14:34 +0530

    update

--- src/Urls.tsx
+++ src/Urls.tsx
@@ -1,7 +1,7 @@
-export const Url = "https://scicure-publications-backend-1.onrender.com/";
+export const Url = "http://15.207.14.102:5001/";
 // export const Url = "http://localhost:5001/";
 
-export const ImageUrl = "https://scicure-publications-backend-1.onrender.com/uploads/";
+export const ImageUrl = "http://15.207.14.102:5001/uploads/";
 // export const ImageUrl = "http://localhost:5001/uploads";
 
 export const URLS = {
